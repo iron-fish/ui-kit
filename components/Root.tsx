80f5550ad90f3c7440d9dbdd324bfba1242d66c1
Nikita Demidovich <demidovich@scand.com>
2022-03-15 08:43:00 +0300

    Added Chakra UI and CoorModeSwitcher.

---
+++ components/Root.tsx
@@ -0,0 +1,23 @@
+import type { FC } from 'react'
+
+import { ChakraProvider, ConfigColorMode, extendTheme } from "@chakra-ui/react"
+import IronFishTheme from "theme/theme"
+
+type RootProps = {
+  initialColorMode?: ConfigColorMode
+}
+
+const Root: FC<RootProps> = ({
+  initialColorMode = 'light',
+  children 
+}) => {
+  const theme = extendTheme(IronFishTheme);
+
+  return <>
+    <ChakraProvider theme={theme}>
+      {children}
+    </ChakraProvider>
+  </>
+}
+
+export default Root
---
+++ components/index.tsx
@@ -0,0 +1,4 @@
+export * from '@chakra-ui/react'
+
+export * from './Root'
+export * from './ColorModeSwitcher'
