f4325863f4a9d37be9f2cea19da9d594fabf566a
Aliaksandr Zhurauski <zhurauski@scand.com>
2022-03-21 18:00:56 +0300

    refactoring.

--- theme/buttonsTheme.ts
+++ theme/buttonsTheme.ts
@@ -2,7 +2,7 @@ import type { ComponentStyleConfig } from '@chakra-ui/theme'
 import { mode } from '@chakra-ui/theme-tools'
 import { NAMED_COLORS } from 'styles/constants'
 
-const buttonsTheme: ComponentStyleConfig  = {
+const buttonsTheme: ComponentStyleConfig = {
   baseStyle: {
     fontStyle: 'normal',
     fontWeight: '400',
