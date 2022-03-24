73e15ed3f4cdc0f042bf22bf72945bc20ff63618
Nikita Demidovich <demidovich@scand.com>
2022-03-24 06:52:59 +0300

    Renamed TextFieldGroup to FieldGroup.

--- stories/Input.stories.tsx
+++ stories/Input.stories.tsx
@@ -2,7 +2,7 @@ import { FC } from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import { Box, Button, Stack } from '@chakra-ui/react'
 import TextField from 'components/TextField'
-import TextFieldGroup from 'components/TextFieldGroup'
+import FieldGroup from 'components/FieldGroup'
 import ColorModeSwitcher from 'components/ColorModeSwitcher'
 
 export default {
@@ -33,22 +33,22 @@ export const BasicExample: ComponentStory<FC> = () => (
     <Box>
       <h3>Text Field Group Example</h3>
       <h4>Buttons on the left</h4>
-      <TextFieldGroup w="50%" my={1}>
+      <FieldGroup w="50%" my={1}>
         <Button>Test 1</Button>
         <Button>Test 2</Button>
         <TextField label="Group Example" />
-      </TextFieldGroup>
+      </FieldGroup>
       <h4>Buttons on the right</h4>
-      <TextFieldGroup w="50%" my={1}>
+      <FieldGroup w="50%" my={1}>
         <TextField label="Group Example" />
         <Button>Test 1</Button>
         <Button>Test 2</Button>
-      </TextFieldGroup>
+      </FieldGroup>
       <h4>Two inputs</h4>
-      <TextFieldGroup w="50%" my={1}>
+      <FieldGroup w="50%" my={1}>
         <TextField label="Group Example 1" />
         <TextField label="Group Example 2" />
-      </TextFieldGroup>
+      </FieldGroup>
     </Box>
   </Stack>
 )
