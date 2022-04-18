05895cd2150439d2191db18dcab9c1251667e44f
Aliaksandr Zhurauski <zhurauski@scand.com>
2022-04-18 15:00:01 +0300

    Improved styles for nav items.

---
+++ svgx/arrow-down-left.tsx
@@ -0,0 +1,12 @@
+export function ArrowDownLeft() {
+  return (
+    <svg width="16" height="17" fill="none" xmlns="http://www.w3.org/2000/svg">
+      <path
+        d="M16 1.598 14.496 0 2.133 13.135V5.667H0V17h10.667v-2.267h-7.03L16 1.598Z"
+        fill="currentColor"
+      />
+    </svg>
+  )
+}
+
+export default ArrowDownLeft
