1affddf80b23f61a0e7782d029a3d67e58343bb7
Aliaksandr Zhurauski <zhurauski@scand.com>
2022-07-12 13:15:02 +0300

    Add types.

--- components/CheckBox.tsx
+++ components/CheckBox.tsx
@@ -1,6 +1,8 @@
-import { Checkbox } from '@chakra-ui/react'
+import { Checkbox, CheckboxProps } from '@chakra-ui/react'
 import CheckedCheckBox from 'svgx/CheckedCheckBox'
 
-const CheckBox = props => <Checkbox icon={<CheckedCheckBox />} {...props} />
+const CheckBox = (props: CheckboxProps) => (
+  <Checkbox icon={<CheckedCheckBox />} {...props} />
+)
 
 export default CheckBox
