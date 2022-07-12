3d24dc63ca3649b469d0344d2e6bb2f103efaee5
Aliaksandr Zhurauski <zhurauski@scand.com>
2022-07-12 11:09:02 +0300

    Fix build

--- components/MnemonicView.tsx
+++ components/MnemonicView.tsx
@@ -61,7 +61,7 @@ const MnemonicInput: FC<MnemonicInputProps> = ({
           {orderNo}
         </Kbd>
         <Input
-          maxlength="8"
+          maxLength="8"
           variant={'unstyled'}
           w={`5.3125rem`}
           value={isReadOnly ? visibleValue : value}
