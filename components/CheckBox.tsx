e77e7bab2b16fb896a7b3da786923d964709ff3f
Aliaksandr Zhurauski <zhurauski@scand.com>
2022-07-12 16:35:03 +0300

    Remove customized checked version of checkbox.

--- components/CheckBox.tsx
+++
@@ -1,8 +0,0 @@
-import { Checkbox, CheckboxProps } from '@chakra-ui/react'
-import CheckedCheckBox from 'svgx/CheckedCheckBox'
-
-const CheckBox = (props: CheckboxProps) => (
-  <Checkbox icon={<CheckedCheckBox />} {...props} />
-)
-
-export default CheckBox
--- stories/CheckBox.stories.tsx
+++ stories/CheckBox.stories.tsx
@@ -1,7 +1,6 @@
 import { FC } from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import { Checkbox, VStack } from '@chakra-ui/react'
-import { CheckBox } from 'components'
 
 export default {
   title: 'Components/Checkboxes',
@@ -10,7 +9,6 @@ export default {
 
 export const Checkboxes: ComponentStory<FC> = () => (
   <VStack spacing={15}>
-    <CheckBox>UI-kit checkbox</CheckBox>
     <Checkbox>Default chakra-ui checkbox</Checkbox>
   </VStack>
 )
