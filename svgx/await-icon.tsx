e38dbabf3167bc7b687083b64fbdfea1794e3a86
Aliaksandr Zhurauski <zhurauski@scand.com>
2022-10-05 16:54:09 +0300

    Add transaction state icons icons.

---
+++ svgx/await-icon.tsx
@@ -0,0 +1,18 @@
+import { Icon, IconProps } from '@chakra-ui/react'
+import { FC } from 'react'
+
+export const AwaitIcon: FC<IconProps> = props => (
+  <Icon
+    w="14px"
+    h="14px"
+    viewBox="0 0 14 14"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+    {...props}
+  >
+    <path d="M7 3.29407V6.99995L9.88235 9.8823" stroke="black" />
+    <circle cx="7" cy="7" r="6.5" stroke="black" />
+  </Icon>
+)
+
+export default AwaitIcon
