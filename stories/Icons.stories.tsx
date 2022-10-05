7ea044ae849b4a3d4b9d962fad269826caaa4125
Aliaksandr Zhurauski <zhurauski@scand.com>
2022-10-05 13:40:49 +0300

    Update Icons story.

--- stories/Icons.stories.tsx
+++ stories/Icons.stories.tsx
@@ -1,61 +1,44 @@
-import { FC, useState } from 'react'
+import { FC } from 'react'
 import { ComponentStory, ComponentMeta } from '@storybook/react'
 import { Flex, Grid, Text, Icon } from '@chakra-ui/react'
 import * as Svgx from 'svgx'
-
-const { IconEye, IconBlinkingEye, ...restSvgx } = Svgx
 
 export default {
   title: 'Components/Icons',
   component: Flex,
 } as ComponentMeta<typeof Icon>
 
-const getIconDemo = (UIKitIcon, iconName) => (
+const getIconDemo = (UIKitIcon, iconName, iconProps = {}) => (
   <Flex direction="column" align="center">
-    <UIKitIcon height="24px" width="auto" />
+    <UIKitIcon height="24px" width="auto" {...iconProps} />
     <Text pt="5px">{iconName}</Text>
   </Flex>
 )
 
+const ICONS_STATE = {
+  IconEye: {
+    propValues: [{ crossed: true }, { crossed: false }],
+  },
+  IconBlinkingEye: {
+    propValues: [{ closed: true }, { closed: false }],
+  },
+}
+
 export const Icons: ComponentStory<FC> = () => {
-  const [iconsState, setIconsState] = useState({
-    IconEye: true,
-    IconBlinkingEye: true,
-  })
   return (
     <Grid
       gap={5}
       gridTemplateColumns="repeat( auto-fit, minmax(150px, 1fr) )"
       m="1rem"
     >
-      {Object.entries(restSvgx).map(([key, IconComponent]) =>
-        getIconDemo(IconComponent, key)
-      )}
-      <Flex direction="column" align="center">
-        <IconEye
-          height="24px"
-          width="auto"
-          crossed={iconsState.IconEye}
-          onClick={() => {
-            setIconsState({ ...iconsState, IconEye: !iconsState.IconEye })
-          }}
-        />
-        <Text pt="5px">IconEye</Text>
-      </Flex>
-      <Flex direction="column" align="center">
-        <IconBlinkingEye
-          height="24px"
-          width="auto"
-          closed={iconsState.IconBlinkingEye}
-          onClick={() => {
-            setIconsState({
-              ...iconsState,
-              IconBlinkingEye: !iconsState.IconBlinkingEye,
-            })
-          }}
-        />
-        <Text pt="5px">IconBlinkingEye</Text>
-      </Flex>
+      {Object.entries(Svgx).map(([key, IconComponent]) => {
+        if (ICONS_STATE[key]) {
+          return ICONS_STATE[key].propValues.map(props =>
+            getIconDemo(IconComponent, key, props)
+          )
+        }
+        return getIconDemo(IconComponent, key)
+      })}
     </Grid>
   )
 }
