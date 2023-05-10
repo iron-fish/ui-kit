87aa80280df68c446fc54888481bc53ca0780e7b
Nikita Demidovich <ndemidovich-sc@users.noreply.github.com>
2023-05-10 16:19:52 +0300

    Added new colors. Updated card variants. (#127)

--- theme/components/Card.ts
+++ theme/components/Card.ts
@@ -5,18 +5,163 @@ import { createMultiStyleConfigHelpers } from '@chakra-ui/styled-system'
 const { definePartsStyle, defineMultiStyleConfig } =
   createMultiStyleConfigHelpers(parts.keys)
 
+const BASE_STYLES = {
+  border: '0.063rem solid',
+  bg: NAMED_COLORS.WHITE,
+  borderColor: `${NAMED_COLORS.LIGHT_GREY} !important`,
+  borderRadius: '0.125rem',
+  _dark: {
+    bg: NAMED_COLORS.DARKER_GREY,
+    borderColor: `${NAMED_COLORS.DARK_GREY} !important`,
+  },
+  _before: {
+    position: 'absolute',
+    content: `""`,
+    bg: 'inherit',
+    h: 'inherit',
+    w: 'inherit',
+    maxH: 'inherit',
+    minH: 'inherit',
+    maxW: 'inherit',
+    minW: 'inherit',
+    height: 'inherit',
+    width: 'inherit',
+    maxHeight: 'inherit',
+    minHeight: 'inherit',
+    maxWidth: 'inherit',
+    minWidth: 'inherit',
+    borderColor: `inherit !important`,
+    borderRadius: 'inherit',
+    border: 'inherit',
+    mr: '-0.25rem',
+    mb: '-0.25rem',
+    mt: '0.25rem',
+    ml: '0.25rem',
+    zIndex: -1,
+  },
+}
+
 const variants = {
   ironFish: definePartsStyle({
+    container: BASE_STYLES,
+    body: {
+      p: 0,
+      flex: 'unset',
+    },
+  }),
+  'ironfish-violet-card': definePartsStyle({
     container: {
-      backgroundColor: NAMED_COLORS.WHITE,
-      boxShadow: `0.25rem 0.25rem 0 -0.063rem ${NAMED_COLORS.WHITE}, 0.25rem 0.25rem ${NAMED_COLORS.LIGHT_GREY}`,
-      border: `0.063rem solid ${NAMED_COLORS.LIGHT_GREY}`,
-      borderRadius: '0.125rem',
-      m: '0 0.2rem 0.2rem 0',
-      _dark: {
-        backgroundColor: NAMED_COLORS.DARKER_GREY,
-        border: `0.063rem solid ${NAMED_COLORS.DARK_GREY}`,
-        boxShadow: `0.25rem 0.25rem 0 -0.063rem ${NAMED_COLORS.DARKER_GREY}, 0.25rem 0.25rem ${NAMED_COLORS.DARK_GREY}`,
+      ...BASE_STYLES,
+      borderRadius: '0.25rem',
+      bg: `linear-gradient(89.56deg, ${NAMED_COLORS.LIGHT_VIOLET} 0.38%, ${NAMED_COLORS.VIOLET} 99.64%)`,
+      borderColor: NAMED_COLORS.BLACK,
+      '&::before': {
+        bg: `linear-gradient(89.56deg, ${NAMED_COLORS.WHITE} 0.38%, ${NAMED_COLORS.VIOLET} 99.64%)`,
+      },
+    },
+    body: {
+      p: 0,
+      flex: 'unset',
+    },
+  }),
+  'ironfish-pistachio-card': definePartsStyle({
+    container: {
+      ...BASE_STYLES,
+      borderRadius: '0.25rem',
+      bg: `linear-gradient(89.56deg, ${NAMED_COLORS.LIGHT_PISTACHIO} 0.38%, ${NAMED_COLORS.PISTACHIO} 99.64%)`,
+      borderColor: NAMED_COLORS.BLACK,
+      '&::before': {
+        bg: `linear-gradient(89.56deg, ${NAMED_COLORS.WHITE} 0.38%, ${NAMED_COLORS.PISTACHIO} 99.64%)`,
+      },
+    },
+    body: {
+      p: 0,
+      flex: 'unset',
+    },
+  }),
+  'ironfish-skyblue-card': definePartsStyle({
+    container: {
+      ...BASE_STYLES,
+      borderRadius: '0.25rem',
+      bg: `linear-gradient(89.56deg, ${NAMED_COLORS.LIGHT_SKY_BLUE} 0.38%, ${NAMED_COLORS.SKY_BLUE} 99.64%)`,
+      borderColor: NAMED_COLORS.BLACK,
+      '&::before': {
+        bg: `linear-gradient(89.56deg, ${NAMED_COLORS.WHITE} 0.38%, ${NAMED_COLORS.SKY_BLUE} 99.64%)`,
+      },
+    },
+    body: {
+      p: 0,
+      flex: 'unset',
+    },
+  }),
+  'ironfish-sandy-card': definePartsStyle({
+    container: {
+      ...BASE_STYLES,
+      borderRadius: '0.25rem',
+      bg: `linear-gradient(89.56deg, ${NAMED_COLORS.SANDY} 0.38%, ${NAMED_COLORS.LIGHT_YELLOW} 99.64%)`,
+      borderColor: NAMED_COLORS.BLACK,
+      '&::before': {
+        bg: `linear-gradient(89.56deg, ${NAMED_COLORS.WHITE} 0.38%, ${NAMED_COLORS.LIGHT_YELLOW} 99.64%)`,
+      },
+    },
+    body: {
+      p: 0,
+      flex: 'unset',
+    },
+  }),
+  'ironfish-pink-card': definePartsStyle({
+    container: {
+      ...BASE_STYLES,
+      borderRadius: '0.25rem',
+      bg: `linear-gradient(89.56deg, ${NAMED_COLORS.LIGHT_PINK} 0.38%, ${NAMED_COLORS.LIGHT_RED} 99.64%)`,
+      borderColor: NAMED_COLORS.BLACK,
+      '&::before': {
+        bg: `linear-gradient(89.56deg, ${NAMED_COLORS.WHITE} 0.38%, ${NAMED_COLORS.LIGHT_RED} 99.64%)`,
+      },
+    },
+    body: {
+      p: 0,
+      flex: 'unset',
+    },
+  }),
+  'ironfish-lemon-card': definePartsStyle({
+    container: {
+      ...BASE_STYLES,
+      borderRadius: '0.25rem',
+      bg: `linear-gradient(89.56deg, ${NAMED_COLORS.LEMON} 0.38%, ${NAMED_COLORS.YELLOW} 99.64%)`,
+      borderColor: NAMED_COLORS.BLACK,
+      '&::before': {
+        bg: `linear-gradient(89.56deg, ${NAMED_COLORS.WHITE} 0.38%, ${NAMED_COLORS.YELLOW} 99.64%)`,
+      },
+    },
+    body: {
+      p: 0,
+      flex: 'unset',
+    },
+  }),
+  'ironfish-beige-card': definePartsStyle({
+    container: {
+      ...BASE_STYLES,
+      borderRadius: '0.25rem',
+      bg: `linear-gradient(89.56deg, ${NAMED_COLORS.LIGHT_BEIGE} 0.38%, ${NAMED_COLORS.BEIGE} 99.64%)`,
+      borderColor: NAMED_COLORS.BLACK,
+      '&::before': {
+        bg: `linear-gradient(89.56deg, ${NAMED_COLORS.WHITE} 0.38%, ${NAMED_COLORS.BEIGE} 99.64%)`,
+      },
+    },
+    body: {
+      p: 0,
+      flex: 'unset',
+    },
+  }),
+  'ironfish-royalblue-card': definePartsStyle({
+    container: {
+      ...BASE_STYLES,
+      borderRadius: '0.25rem',
+      bg: `linear-gradient(89.56deg, ${NAMED_COLORS.LIGHT_ROYAL_BLUE} 0.38%, ${NAMED_COLORS.ROYAL_BLUE} 99.64%)`,
+      borderColor: NAMED_COLORS.BLACK,
+      '&::before': {
+        bg: `linear-gradient(89.56deg, ${NAMED_COLORS.WHITE} 0.38%, ${NAMED_COLORS.ROYAL_BLUE} 99.64%)`,
       },
     },
     body: {
--- theme/constants.ts
+++ theme/constants.ts
@@ -19,13 +19,38 @@ export const NAMED_COLORS: Record<string, string> = {
   // brand
   LIGHT_BLUE: '#2C72FF',
   BLUE: '#1D0070',
-  LIGHT_YELLOW: '#FFCD85',
-  YELLOW: '#FFEC1F',
-  LIGHT_RED: '#FFC2E8',
   RED: '#F15929',
   DARKER_GREY_1: '#222222',
   DARKER_GREY_2: '#626262',
   LIGHT_BLACK: '#101010',
+  // Card 1
+  LIGHT_VIOLET: '#F4BFFF',
+  VIOLET: '#DE83F0',
+  // Card 2
+  LIGHT_PISTACHIO: '#E1FFB0',
+  PISTACHIO: '#C7F182',
+  // Card 3
+  LIGHT_SKY_BLUE: '#BFF6FF',
+  SKY_BLUE: '#8AE1EF',
+  // Card 4
+  SANDY: '#FFE2B9',
+  LIGHT_YELLOW: '#FFCD85',
+  // Card 5
+  LIGHT_PINK: '#FFD7F0',
+  LIGHT_RED: '#FFC2E8',
+  // Card 6
+  LEMON: '#FFF698',
+  YELLOW: '#FFEC1F',
+  // Card 7
+  LIGHT_BEIGE: '#FFF5EB',
+  BEIGE: '#FBEADA',
+  // Card 8
+  LIGHT_ROYAL_BLUE: '#85ADFD',
+  ROYAL_BLUE: '#4B87FF',
+  // Node synced status
+  LIGHT_GRASS: '#ECFFCE',
+  GRASS: '#6A991C',
+  DARK_GRASS: '#242C18',
 }
 
 export enum FONTS {
