3c417e464a9fa1bb3c2d2f752a6f93c9b4c0e0a9
Daniel Cortes <danielgcortes@gmail.com>
2023-07-05 14:23:05 -0600

    Dark mode

--- svgx/common/three-dots-icon.tsx
+++ svgx/common/three-dots-icon.tsx
@@ -15,7 +15,7 @@ export const ThreeDotsIcon: FC<IconProps> = props => (
     {...props}
   >
     <path
-      fill="#0D0C22"
+      fill="currentColor"
       d="M2 0C.9 0 0 .9 0 2s.9 2 2 2 2-.9 2-2-.9-2-2-2Zm12 0c-1.1 0-2 .9-2 2s.9 2 2 2 2-.9 2-2-.9-2-2-2ZM8 0C6.9 0 6 .9 6 2s.9 2 2 2 2-.9 2-2-.9-2-2-2Z"
     />
   </Icon>
